3fb3bde0920019de836f993148982e4b6c457cca
sasikanthkumar <kumarsasikanth@gmail.com>
2015-06-14 12:15:16 +0530

    added BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,45 @@
+/*
+ This algorithm repeatedly compares adjacent elements and swaps them if they are in wrong order. The comparision is repeated until no swaps are needed which indicates that the list is sorted.
+*/
+
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int *arr,int length);
+
+int main(){
+    
+    int arr[] = {7,4,5,6,2,3};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    
+    bubble_sort(arr,length);
+    
+    int i;
+    
+    for (i=0; i<length; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << "\n";
+
+    return 0;
+}
+
+/* Here we sort the array in increasing order so when the first element is greater than the adjacent element we swap them
+    After every iteration the largest element in the list will be placed to its position in the sorted order
+*/
+
+void bubble_sort(int *arr, int length){
+    int done,i,temp;
+    do{
+        done = 1;
+        for (i=0; i<length-1; i++) {
+            if (arr[i]>arr[i+1]) {
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                done = 0;
+            }
+        }
+    }while (done == 0);
+}
