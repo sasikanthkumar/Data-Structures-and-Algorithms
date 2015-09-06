ad17f42d6e1516f5f85d72205f57377ef9afc205
sasikanthkumar <kumarsasikanth@gmail.com>
2015-09-06 16:36:10 +0530

    final

--- CandidateElimination.cpp
+++ CandidateElimination.cpp
@@ -13,7 +13,7 @@ int main(){
     scanf("%d",&n);
     scanf("%d",&m);
     
-    
+    int exceptional_case = 0;
     vector<string> attr_names(m);
     
     map<string,int> *attr_values = new map<string,int>[m];
@@ -101,12 +101,12 @@ int main(){
             }
         }
         //printing curreent data
-        
+        /**
         for (int k = 0; k<curr_data.size(); k++) {
             cout << curr_data[k] << " ";
         }
         cout << endl;
-        
+        **/
         vector<int> first_step;
         first_step.clear();
         if(curr_data.back() == attr_values[m-1]["Yes"]){    //positive example
@@ -125,8 +125,78 @@ int main(){
                 }
                 first_step.clear();
             }
+            if(generic_boundary.size() == 0){
+                exceptional_case = 1;
+                break;
+            }
             
+            set<vector<int> > consistent_in_s;
             
+            for (set<vector<int> >::iterator it = specific_boundary.begin(); it != specific_boundary.end(); it++) {
+                first_step = *(it);
+                int flag = 1;
+                for (int i = 0; i<(m-1); i++) {
+                    if (first_step[i] == 0) {
+                        flag = 0; // inconsistent
+                        break;
+                    }else if(first_step[i] != -2 && first_step[i] != curr_data[i]){
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1) {
+                    consistent_in_s.insert(first_step);
+                }else{  // inconsistent
+                    for (int i = 0; i<(m-1); i++) {
+                        if (first_step[i] == 0) {
+                            first_step[i] = curr_data[i];
+                        }else if(first_step[i] != curr_data[i]){
+                            first_step[i] = -2;
+                        }
+                    }
+                    consistent_in_s.insert(first_step);
+                }
+            }
+            
+            // remove from s that is more general
+            set<vector<int> > more_general_in_s;
+            vector<int> second_step;
+            for (set<vector<int> >::iterator it1 = consistent_in_s.begin(); it1 != consistent_in_s.end(); it1++) {
+                first_step = *(it1);
+                for (set<vector<int> >::iterator it2 = consistent_in_s.begin(); it2 != consistent_in_s.end(); it2++) {
+                    if (it1 == it2) {
+                        continue;
+                    }
+                    second_step = *(it2);
+                    int flag = 0;
+                    for (int k = 0; k<(m-1); k++) {
+                        if (first_step[k] == 0) {
+                            flag = 1;
+                            break;
+                        }else if((first_step[k] != -2) && (second_step[k] == -2)){
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 1) {
+                        more_general_in_s.insert(second_step);
+                    }
+                    
+                }
+                
+            }
+            
+            specific_boundary.clear();
+            for (set<vector<int> >::iterator it = consistent_in_s.begin(); it != consistent_in_s.end(); it++) {
+                first_step = *(it);
+                if (more_general_in_s.find(first_step) == more_general_in_s.end()) {
+                    specific_boundary.insert(first_step);
+                }
+            }
+            if (specific_boundary.size() == 0) {
+                exceptional_case = 1;
+                break;
+            }
             
         }else{          // negative example
             //cout << "Negative example" <<endl;
@@ -145,14 +215,201 @@ int main(){
                 }
                 first_step.clear();
             }
+            if (specific_boundary.size() == 0) {
+                exceptional_case = 1;
+                break;
+            }
             
+            //removing inconsistent hypothesis in g
+            //if removed adding next consistent hypo in g
+            set <vector<int> > consistent_in_g;
+            consistent_in_g.clear();
+            for (set<vector<int> >::iterator it = generic_boundary.begin(); it != generic_boundary.end(); it++) {
+                
+                first_step = *(it);
+                int flag = 0;
+                for (int i = 0; i<(m-1); i++) {
+                    if (first_step[i] != -2 && first_step[i] != curr_data[i]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 1) {
+                    //consistent
+                    consistent_in_g.insert(first_step);
+                }else{
+                    //inconsistent
+                    //removing and adding next specific and consistent with training ex
+                    vector<int> temporary_hypothesis;
+                    for (int i =0; i<(m-1); i++) {
+                        if (first_step[i] == -2) {
+                            temporary_hypothesis = first_step;
+                            for (int j = 1; j<=attr_values[i].size(); j++) {
+                                if (j == curr_data[i]) {
+                                    continue;
+                                }else{
+                                    temporary_hypothesis[i] = j;
+                                    int flag2 = 1;
+                                    vector<int> first_specific = *(specific_boundary.begin());
+                                    for (int k = 0; k<(m-1); k++) {
+                                        if (temporary_hypothesis[k] != -2 && temporary_hypothesis[k] != first_specific[k] && first_specific[k] != 0) {
+                                            flag2  =0;
+                                            break;
+                                            
+                                        }
+                                    }
+                                    if (flag2 == 1) {
+                                        consistent_in_g.insert(temporary_hypothesis);}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            generic_boundary.clear();
+            set<vector<int> > more_specific_in_consistent_g;
+            vector<int> second_step;
+            for (set<vector<int> >::iterator it1 = consistent_in_g.begin(); it1 != consistent_in_g.end(); it1++) {
+                first_step = *(it1);
+                for (set<vector<int> >::iterator it2 = consistent_in_g.begin(); it2 != consistent_in_g.end(); it2++) {
+                    if (it1 == it2) {
+                        continue;
+                    }
+                    second_step = *(it2);
+                    int flag3 = 1;
+                    for (int k =0; k<(m-1); k++) {
+                        if (first_step[k] != -2 && first_step[k]!=second_step[k]) {
+                            flag3 = 0;
+                            break;
+                        }
+                    }
+                    if (flag3 == 1) {
+                        more_specific_in_consistent_g.insert(second_step);
+                    }
+                }
+            }
+            
+            for (set<vector<int> >::iterator it = consistent_in_g.begin(); it != consistent_in_g.end(); it++) {
+                first_step = *(it);
+                if (more_specific_in_consistent_g.find(first_step) == more_specific_in_consistent_g.end()) {
+                    
+                    generic_boundary.insert(first_step);
+                }
+            }
             
             
             
         }
         
+        if (specific_boundary == generic_boundary) {
+            exceptional_case = 1;
+            break;
+        }
+        
+        
     }
+    if (exceptional_case == 1) {
+        if (specific_boundary.size() == 0 || generic_boundary.size() == 0) {
+            cout << "Specific and generic boundaries converged to NULL" << endl;
+        }else if(specific_boundary == generic_boundary){
+            cout << "Specific and generic boundaries are equal" << endl;
+            vector<int> printing_eq;
+            cout << endl;
+            for (set<vector<int> >::iterator it = specific_boundary.begin(); it != specific_boundary.end() ; it++) {
+                printing_eq = *(it);
+                cout << "< ";
+                for (int i = 0; i<(m-1); i++) {
+                    if (printing_eq[i] == 0) {
+                        cout << "#";
+                    }else if(printing_eq[i] == -2){
+                        cout << "?";
+                    }else{
+                        for (map<string,int>::iterator it1 = attr_values[i].begin(); it1 != attr_values[i].end(); it1++) {
+                            if ((*it1).second == printing_eq[i]) {
+                                cout << (*it1).first;
+                            }
+                        }
+                    }
+                    if (i != (m-2)) {
+                        cout << " ,";
+                    }
+                }
+                cout << " > ";
+                
+            }
+            cout << endl;
+            cout << endl;
+        }
+    }else{
+        //we got version space printing left
+        cout << endl;
+        cout << "Printing specific boundary" << endl;
+        cout << endl;
+        //printing specific border
+        vector<int> printing_s;
+        for (set<vector<int> >::iterator it = specific_boundary.begin(); it != specific_boundary.end(); it++) {
+            printing_s = *(it);
+            cout << "< ";
+            for (int i = 0; i<(m-1); i++) {
+                //cout << printing_s[i] << " ";
+                if (printing_s[i] == 0) {
+                    cout << "#";
+                }else if(printing_s[i] == -2){
+                    cout << "?";
+                }else{
+                    for (map<string,int>::iterator it1 = attr_values[i].begin(); it1 != attr_values[i].end(); it1++) {
+                        if ((*it1).second == printing_s[i]) {
+                            cout << (*it1).first;
+                        }
+                    }
+                }
+                if (i != (m-2)) {
+                    cout << " ,";
+                }
+            }
+            cout << " > " ;
+        }
+        
+        cout << endl;
+        cout << endl;
+        
+        cout << "Printing Generic border" << endl;
+        cout << endl;
+        
+        vector<int> printing_g;
+        //cout << generic_boundary.size() << endl;
+        printing_g = *(generic_boundary.begin());
+        //cout << printing_g.size() << endl;
+        
+        for (set<vector<int> >::iterator it = generic_boundary.begin(); it != generic_boundary.end(); it++) {
+            printing_g = *(it);
+            cout << "< ";
+            for (int i = 0; i<(m-1); i++) {
+                if (printing_g[i] == 0) {
+                    cout << "#";
+                }else if(printing_g[i] == -2){
+                    cout << "?";
+                }else{
+                    for (map<string,int>::iterator it1 = attr_values[i].begin(); it1 != attr_values[i].end(); it1++) {
+                        if ((*it1).second == printing_g[i]) {
+                            cout << (*it1).first;
+                        }
+                    }
+                    
+                }
+                
+                if (i != (m-2)) {
+                    cout << " ,";
+                }
+            }
+            cout << " > ";
+        }
+        cout << endl;
+        cout << endl;
+        
     
+    }
     
     
     return 0;
